c93527305e00a4e4f807d06d7120c22d3c1854af
Maxim Elushev <68665764+Flockk@users.noreply.github.com>
2022-12-25 14:28:30 +0300

    Добавил комментарии

--- Lab6/Lab6_3.asm
+++ Lab6/Lab6_3.asm
@@ -3,39 +3,39 @@ TEXT SEGMENT 'CODE'
      ORG 100h
 MAIN PROC
 
-OPENFILE:
-     MOV AH, 3Dh         
-     MOV AL, 2           
-     LEA DX, FILENAME    
-     INT 21h             
-     MOV HANDLE, AX      
+OPENFILE:                                   ; Открытие файла      
+     MOV AH, 3Dh                            ; Пересылка в AH функции 3Dh открытия файла         
+     MOV AL, 2                              ; Пересылка в AL режима доступа (2 - запись и чтение)           
+     LEA DX, FILENAME                       ; Загружаем в dx адрес с именем файла    
+     INT 21h                                ; Функция прерывания             
+     MOV HANDLE, AX                         ; Handle - переменная для записи номера файла      
 
-ENDFILE:
-     MOV AH, 42h
-     MOV AL, 02          
-     MOV BX, HANDLE  
-     MOV CX, 0           
-     MOV DX, 0           
-     INT 21h
+ENDFILE:                                    ; Установка указателя на конец файла
+     MOV AH, 42h                            ; Функция для установки указателя
+     MOV AL, 02                             ; Режим установки указателя - знаковое смещение от конца файла          
+     MOV BX, HANDLE                         ; Установливаем дескриптор   
+     MOV CX, 0                              ; Старшая часть смещения           
+     MOV DX, 0                              ; Младшая часть смещения           
+     INT 21h                                ; Функция прерывания
 
-WRITESTR:
-     MOV AH, 40h
-     MOV BX, HANDLE      
-     MOV CX, STRING_LEN  
-     LEA DX, STRING      
-     INT 21h
+WRITESTR:                                   ; Запись строки
+     MOV AH, 40h                            ; Функция вывода строки
+     MOV BX, HANDLE                         ; Установливаем дескриптор      
+     MOV CX, STRING_LEN                     ; Число пересылаемых байт  
+     LEA DX, STRING                         ; Адрес буфера      
+     INT 21h                                ; Функция прерывания
 
-EXIT:
-     MOV AH, 3Eh     
-     MOV BX, HANDLE
-     INT 21h
-     MOV AX, 4C00h         
-     INT 21h 
+EXIT:                                       ; Закрытие файла
+     MOV AH, 3Eh                            ; Пересылка в AH функции 3Eh закрытия файла     
+     MOV BX, HANDLE                         ; Установливаем дескриптор
+     INT 21h                                ; Функция прерывания
+     MOV AX, 4C00h                          ; Пересылка в AX функции 4C00h закрытия программы         
+     INT 21h                                ; Функция прерывания 
 
 MAIN ENDP
-     FILENAME DB 'file1.txt', 0
-     HANDLE DW ?
-     STRING DB 'Bye, world!', 0Dh, 0Ah
-     STRING_LEN EQU $-STRING
+     FILENAME DB 'file1.txt', 0             ; Имя файла
+     HANDLE DW ?                            ; Ячейка для дескриптора
+     STRING DB 'Bye, world!', 0Dh, 0Ah      ; Строка для записи в файл
+     STRING_LEN EQU $-STRING                ; Длина строки
      TEXT ENDS
 END  MAIN
