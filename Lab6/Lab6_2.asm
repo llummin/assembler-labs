c5ebe128fa02da356f603bc734e9bcfdd8f07084
Maxim Elushev <68665764+Flockk@users.noreply.github.com>
2022-12-25 14:09:10 +0300

    Update Lab6_2.asm

--- Lab6/Lab6_2.asm
+++ Lab6/Lab6_2.asm
@@ -3,37 +3,37 @@ TEXT SEGMENT 'CODE'
      ORG 100h
 MAIN PROC
 
-OPENFILE:
-     MOV AH, 3Dh
-     MOV AL, 2
-     LEA DX, FILENAME
-     INT 21h
-     MOV HANDLE,AX	
+OPENFILE:                           ; Открытие файла
+     MOV AH, 3Dh                    ; Пересылка в AH функции 3Dh открытия файла
+     MOV AL, 2                      ; Пересылка в AL режима доступа (2 - запись и чтение) 
+     LEA DX, FILENAME               ; Загружаем в dx адрес с именем файла
+     INT 21h                        ; Функция прерывания
+     MOV HANDLE, AX                 ; Handle - переменная для записи номера файла 	
 
-READFILE:
-     MOV AH, 3Fh
-     MOV BX, HANDLE
-     MOV CX, 80	
-     LEA DX, BUFIN	
-     INT 21h
-     MOV CX, AX	
+READFILE:                           ; Чтение файла
+     MOV AH, 3Fh                    ; Пересылка в AH функции 3Fh чтения файла
+     MOV BX, HANDLE                 ; Установливаем дескриптор
+     MOV CX, 80                     ; Прочитать 80 байт	
+     LEA DX, BUFIN                  ; В область ввода	 
+     INT 21h                        ; Функция прерывания
+     MOV CX, AX                     ; Столько прочитали	
 
-WRITELINE:
-     MOV AH, 40h
-     MOV BX, 1	
-     LEA DX, BUFIN	
-     INT 21h
+WRITELINE:                          ; Вывод прочитанного на экран
+     MOV AH, 40h                    ; Функция вывода
+     MOV BX, 1	                     ; Дескриптор экрана
+     LEA DX, BUFIN                  ; Отсюда выводим CX байт	
+     INT 21h                        ; Функция прерывания
 
-EXIT:
-     MOV AH, 3Eh     
-     MOV BX, HANDLE
-     INT 21h
-     MOV AX, 4C00h         
-     INT 21h 
+EXIT:                               ; Закрытие файла
+     MOV AH, 3Eh                    ; Пересылка в AH функции 3Eh закрытия файла     
+     MOV BX, HANDLE                 ; Установливаем дескриптор
+     INT 21h                        ; Функция прерывания
+     MOV AX, 4C00h                  ; Пересылка в AX функции 4C00h закрытия программы
+     INT 21h                        ; Функция прерывания 
 
 MAIN ENDP
-     FILENAME DB 'file1.txt', 0
-     HANDLE DW ?
-     BUFIN DB 80 DUP(' ')
+     FILENAME DB 'file1.txt', 0     ; Имя файла
+     HANDLE DW ?                    ; Ячейка для дескриптора
+     BUFIN DB 80 DUP(' ')           ; Область ввода
      TEXT ENDS
 END  MAIN
