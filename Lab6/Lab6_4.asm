b3a55abba1e86495295fc81703c7b7c8c01f9fd3
Maxim Elushev <68665764+Flockk@users.noreply.github.com>
2022-12-25 14:42:24 +0300

    Добавил комментарии

--- Lab6/Lab6_4.asm
+++ Lab6/Lab6_4.asm
@@ -3,24 +3,24 @@ TEXT SEGMENT 'CODE'
      ORG 100h
 MAIN PROC
 
-RENAME:    
-     MOV AH,56H
-     LEA DX,OLDNAME
-     LEA DI,NEWNAME
-     INT 21h
+RENAME:                                ; Переименование файла    
+     MOV AH, 56H                       ; Пересылка в AH функции 56H переименования файла
+     LEA DX, OLDNAME                   ; Загружаем в DX адрес со старым именем файла
+     LEA DI, NEWNAME                   ; Загружаем в DI адрес с новым именем файла
+     INT 21h                           ; Функция прерывания
 
-DEL:
-     MOV AH,41H
-     LEA DX,DELNAME
-     INT 21h
+DEL:                                   ; Удаление файла
+     MOV AH, 41H                       ; Пересылка в AH функции 41H удаления файла
+     LEA DX, DELNAME                   ; Загружаем в DX адрес с именем удаляемого файла                   
+     INT 21h                           ; Функция прерывания
 
-EXIT:
-     MOV AX, 4C00h         
-     INT 21h
+EXIT:                                  ; Закрытие файла
+     MOV AX, 4C00h                     ; Пересылка в AX функции 4C00h закрытия программы         
+     INT 21h                           ; Функция прерывания
 
 MAIN ENDP
-     OLDNAME DB	'file1.txt',0	
-     NEWNAME DB	'file2.txt',0	
-     DELNAME DB	'file3.txt',0	
+     OLDNAME DB	'file1.txt',0      ; Старое имя файла	
+     NEWNAME DB	'file2.txt',0      ; Новое имя файла	
+     DELNAME DB	'file3.txt',0      ; Имя удаляемого файла	
      TEXT ENDS
 END  MAIN
